7caa05181d2870ecb31d7c9b7764da4398a07558
slyx2017 <208106500@qq.com>
2017-06-04 11:01:08 +0800

    ivon-clock add

--- WebAppMvc/Content/js/commjs.js
+++ WebAppMvc/Content/js/commjs.js
@@ -1,6 +1,7 @@
 ﻿$(function () {
     TabsMenuRight();
     createAccrodion();
+    clockon();
 });
 /** 
  * 更换EasyUI主题的方法 
@@ -214,6 +215,32 @@ function TabsMenuRight() {
     $("#m-exit").click(function () {
         $('#tabsMenu').menu('hide');
     })
+}
+//日期时间显示timper
+function clockon() {
+    var now = new Date();
+    var year = now.getFullYear(); //getFullYear getYear
+    var month = now.getMonth();
+    var date = now.getDate();
+    var day = now.getDay();
+    var hour = now.getHours();
+    var minu = now.getMinutes();
+    var sec = now.getSeconds();
+    var week;
+    month = month + 1;
+    if (month < 10) month = "0" + month;
+    if (date < 10) date = "0" + date;
+    if (hour < 10) hour = "0" + hour;
+    if (minu < 10) minu = "0" + minu;
+    if (sec < 10) sec = "0" + sec;
+    var arr_week = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
+    week = arr_week[day];
+    var time = "";
+    time = year + "年" + month + "月" + date + "日" + " " + hour + ":" + minu + ":" + sec + " " + week;
+
+    $("#bgclock").html(time);
+
+    var timer = setTimeout("clockon()", 200);
 }
 //dialog遮罩层全局
 function maskInit() {
